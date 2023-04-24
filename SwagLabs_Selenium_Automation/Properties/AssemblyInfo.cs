e95b1fadde2220edcbdb2668b7f57ff8dade58ff
MujahidAkberAli <mujahidakberali@gmail.com>
2023-04-24 22:58:57 +0500

    Add project files.

---
+++ SwagLabs_Selenium_Automation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SwagLabs_Selenium_Automation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SwagLabs_Selenium_Automation")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("990bc913-055d-4abd-ae2e-39eb25cad372")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SwagLabs_Selenium_Automation/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+
+namespace SwagLabs_Selenium_Automation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new EdgeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.saucedemo.com/";
+            driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
+            driver.FindElement(By.Id("password")).SendKeys("secret_sauce");
+            driver.FindElement(By.Id("login-button")).Click();
+            var select = driver.FindElement(By.ClassName("product_sort_container"));
+            var selectDropDown = new SelectElement(select);
+            selectDropDown.SelectByValue("lohi");
+            driver.FindElement(By.Id("add-to-cart-sauce-labs-onesie")).Click();
+            driver.FindElement(By.ClassName("shopping_cart_link")).Click();
+            driver.FindElement(By.Id("checkout")).Click();
+            driver.FindElement(By.Id("first-name")).SendKeys("Mujahid");
+            driver.FindElement(By.Id("last-name")).SendKeys("Akber Ali");
+            driver.FindElement(By.Id("postal-code")).SendKeys("78600");
+            driver.FindElement(By.Id("continue")).Click();
+            driver.FindElement(By.Id("finish")).Click();
+            string actualText = driver.FindElement(By.ClassName("complete-header")).Text;
+            Assert.AreEqual("Thank you for your order!", actualText);
+            driver.FindElement(By.Id("back-to-products")).Click();
+            driver.Close();
+        }
+    }
+}
